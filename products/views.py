361b55cf462d44bd58a07f8df6430cf5bff23326
ma <ma@maui-MacBook-Pro.local>
2022-05-04 09:57:56 +0900

    Modify Live Code Review

--- products/views.py
+++ products/views.py
@@ -14,17 +14,14 @@ class MainView(View):
         try:
             products = Product.objects.all()
             
-            recommend_list = []
-
-            for product in products:
-                recommend_list.append( 
+            recommend_list = [
                 {
                     'id'             : product.id,
                     'name'           : product.name,
                     'product_images' : product.productimage_set.get().product_image_url,
-                    'skintype'       : [types.skin.skin_type for types in product.productskin_set.all()]
-                }
-            )
+                    'skin_type'      : [skin.skin_type for skin in product.skins.all()]
+                }for product in products]
+            
             return JsonResponse({'result' : recommend_list}, status = 200)
         except KeyError:
             return JsonResponse({'message' : 'KEY_ERROR'} , status = 401)
@@ -46,32 +43,35 @@ class ProductlistView(View):
             if name:
                 products = Product.objects.filter(name__icontains=name)
 
-            # main_category_id if문을 통해 (http://10.58.0.93:8000/products/list?main_category_id=1) 이렇게 넣었을때 
-            # main_category_id가 1인 데이터를 가져올 수 있을줄 알았는데 못가져오고 있어서 한번더 확인중입니다.
             if main_category_id:
-                categories = Category.objects.filter(main_category_id=main_category_id)
-                products   = []
-                for category in categories:
-                    products = list(Product.objects.filter(category_id=category.id))
+                categories = Category.objects.filter(main_category__id=main_category_id)
+                products = Product.objects.filter(category__main_category__id=main_category_id)
 
             if category_id:
-                products = Product.objects.filter(category_id=category_id)
+                products = Product.objects.filter(category_id=category_id)            
+                categories = Category.objects.filter(id=category_id)
+            
+            # categories = {}
+            #     if product.category.name in categories:
+            #         categories.append(product.category.name)
+            #     else:
+            #         
                 
-                product_list = []
-                for product in products:
-                    product_list.append(
-                        {
-                            'id'                      : product.id,
-                            'category_name'           : product.category.name,
-                            'category_description'    : product.category.main_description,
-                            'category_subdescription' : product.category.sub_description,
-                            'product_name'            : product.name,
-                            'product_price'           : [selection.price for selection in product.selection_set.all()],
-                            'product_size'            : [selection.size for selection in product.selection_set.all()],
-                            'product_images'          : product.productimage_set.get().product_image_url
-                        }       
-                )
-                return JsonResponse({'result' : product_list}, status = 200)
+            product_list = [{
+                    
+                    'category_name'           : category.name,
+                    'category_description'    : category.main_description,
+                    'category_subdescription' : category.sub_description,
+                    'product' :[{
+                        'id'                      : product.id,
+                        'category_id'             : product.category.id,
+                        'product_name'            : product.name,
+                        'product_price'           : [selection.price for selection in product.selection_set.all()],
+                        'product_size'            : [selection.size for selection in product.selection_set.all()],
+                        'product_images'          : product.productimage_set.get().product_image_url
+                        }for product in products]
+                }for category in categories]
+            return JsonResponse({'result' : product_list}, status = 200)
         except KeyError:
             return JsonResponse({'message' : 'KEY_ERROR'} , status = 401)
         except Product.DoesNotExist:
@@ -86,30 +86,27 @@ class ProductDetailView(View):
         # 4. 가져온 데이터를 append로 product_detail에 모아준다.
         # 4. 성공적으로 가져왔을 경우 filter(id = product_id)에 맞는 product_detail을 전달한다.
         try:
-            products    = Product.objects.filter(id = product_id)
-            product_detail = []
-            
-            for product in products:
-                product_detail.append(
-                    {
-                        'id'                : product.id,
-                        'name'              : product.name,
-                        'texture'           : product.texture,
-                        'sense'             : product.sense,
-                        'detail'            : product.detail,
-                        'direction'         : product.direction,
-                        'usage'             : product.usage,
-                        'category_name'     : product.category.name,
-                        'price'             : [selection.price for selection in product.selection_set.all()],
-                        'size'              : [selection.size for selection in product.selection_set.all()],
-                        'product_images'    : product.productimage_set.get().product_image_url,
-                        'texture_images'    : product.productimage_set.get().texture_image_url,
-                        'skintype'          : [types.skin.skin_type for types in product.productskin_set.all()],
-                        'scent'             : [scent.scent.name for scent in product.productscent_set.all()],
-                        'ingredient_name'   : product.ingredient_set.get().name,
-                        'detail_text'       : product.ingredient_set.get().detail_text
-                    }
-                )
+            products = Product.objects.filter(id = product_id)
+
+            product_detail = [
+                {
+                    'id'                : product.id,
+                    'name'              : product.name,
+                    'texture'           : product.texture,
+                    'sense'             : product.sense,
+                    'detail'            : product.detail,
+                    'direction'         : product.direction,
+                    'usage'             : product.usage,
+                    'category_name'     : product.category.name,
+                    'price'             : [selection.price for selection in product.selection_set.all()],
+                    'size'                          : [selection.size for selection in product.selection_set.all()],
+                    'product_images'    : product.productimage_set.get().product_image_url,
+                    'texture_images'    : product.productimage_set.get().texture_image_url,
+                    'skintype'          : [skin.skin_type for skin in product.skins.all()],
+                    'scent'             : [scent.name for scent in product.scents.all()],
+                    'ingredient_name'   : product.ingredient_set.get().name,
+                    'detail_text'       : product.ingredient_set.get().detail_text
+                } for product in products]
             return JsonResponse({"results": product_detail}, status=200) 
         except KeyError:
             return JsonResponse({'message' : 'KEY_ERROR'} , status = 401)
