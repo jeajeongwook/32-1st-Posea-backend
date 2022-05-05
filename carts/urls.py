c32e6dfea8da63f997a9bbe074cefd2c943c578e
ma <ma@maui-MacBook-Pro.local>
2022-05-05 13:51:54 +0900

    carts view Correction

--- carts/urls.py
+++ carts/urls.py
@@ -3,6 +3,6 @@ from .views import CartView
 
 urlpatterns = [
     path('', CartView.as_view()),
-    path('/<int:cart_id>', CartView.as_view())
+    path('/cart/<int:cart_id>', CartView.as_view())
 
 ]
--- carts/views.py
+++ carts/views.py
@@ -14,13 +14,17 @@ class CartView(View):
         try:
             data = json.loads(request.body)
 
-            Cart.objects.create(
+            cart, created  = Cart.objects.get_or_create(
                 user       = request.user,
                 product_id = data['product_id']
-                
             )
+            if not created and cart.count < 20:
+                cart.count += 1
+                cart.save()
 
-            return JsonResponse({'message': 'SUCCESS'}, status=200)
+                return JsonResponse({'message': 'SUCCESS'}, status=200)
+            elif not created and cart.count > 19:
+                return JsonResponse({'message': 'INVALID_COUNT'}, status=200)
 
         except Cart.DoesNotExist:
             return JsonResponse({'message': 'CART_DOES_NOT_EXIT'}, status=400)
@@ -29,24 +33,41 @@ class CartView(View):
 
     @author
     def get(self, request):
+
+        user  = request.user
+        carts = Cart.objects.filter(user=user)
+
+        if not carts.exists():
+            return JsonResponse({'message': 'INVALID_USER'}, status=400)
+
+        cart_list = [{
+            'user_id'      : user.id,
+            'cart_id'      : cart.id,
+            'count'        : cart.count,
+            'product_id'   : cart.product.id,
+            'product_name' : cart.product.name,
+            'product_size' : cart.product.selection.size,
+            'totalPrice'   : int(cart.count * cart.product.selection.price)
+        } for cart in carts]
+        return JsonResponse({'result': cart_list}, status=200)
+
+    @author
+    def patch(self, request, cart_id):
         try:
-            user  = request.user
-            carts = Cart.objects.filter(user=user)
+            data = json.loads(request.body)
 
-            cart_list = [{
-                'user_id'      : user.id,
-                'cart_id'      : cart.id,
-                'count'        : cart.count,
-                'product_id'   : cart.product.id,
-                'product_name' : cart.product.name,
-                'product_size' : [selection.size for selection in cart.product.selection_set.all()],
-                'totalPrice'   : int(cart.count * [selection.price for selection in cart.product.selection_set.all()])
-            } for cart in carts]
+            if data['count'] <= 0 or data['count'] >= 21:
+                return JsonResponse({'message': 'INVALID_COUNT'}, status=400)
 
-            return JsonResponse({'result': cart_list}, status=200)
+            if Cart.objects.filter(id=cart_id).exists():
+                cart = Cart.objects.get(id=cart_id, user=request.user
+                )
+                cart.count = data['count']
+                cart.save()
+                return JsonResponse({'message': 'COUNT_CHANGED'}, status=201)
 
-        except Cart.DoesNotExist:
-            return JsonResponse({'message': 'CART_DOES_NOT_EXIT'}, status=400)
+            return JsonResponse({'message': 'CART_DOES_NOT_EXIT'}, status=404)
+
         except KeyError:
             return JsonResponse({'message': 'KEY_ERROR'}, status=400)
 
